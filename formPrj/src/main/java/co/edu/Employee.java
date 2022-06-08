272eab8f736b0fde19c03fd0bc58efbe97d095c6
kimnahee <knh6851@naver.com>
2022-06-08 16:34:35 +0900

    20220608 이클립스

---
+++ formPrj/src/main/java/co/edu/Employee.java
@@ -0,0 +1,42 @@
+package co.edu;
+
+public class Employee {
+	//필드
+	private int empId; //private - 접근불가능 / get, set으로 접근
+	private String lastName;
+	private String email;
+	private String jobId;
+	private String hireDate;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
+	}
+	
+	
+}
